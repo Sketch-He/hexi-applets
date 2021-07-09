443a061b82e6e72d51e86ff483056e73eeb53ddb
Sketch-He <2454213572@qq.com>
2021-07-09 14:41:02 +0800

    测试

---
+++ src/pages/userlogin.vue
@@ -0,0 +1,23 @@
+<template>
+  <view>
+    <button size="mini">按钮</button>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "userlogin",
+  data() {
+    return {
+      username: "admin_lzbx",
+      password: "123456",
+    };
+  },
+}
+</script>
+
+<style scoped>
+.main {
+  text-align: center;
+}
+</style>
